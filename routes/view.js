378cf819cccbd22439f282e9d8b2fe97ac209610
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-12-03 13:00:19 -0500

    logout feature

--- routes/view.js
+++ routes/view.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const { head } = require('./api');
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 var router = express.Router();
 
 
@@ -12,4 +14,10 @@ router.get('/register', function(req, res, next) {
 router.get('/addRestaurant', function(req, res, next) {
   res.render('addRestaurant',{title:'AddRestaurant'});
 })
+
+router.get('/logout', (req, res, next) => {
+  res.clearCookie('token');
+  res.redirect('/');
+})
+
 module.exports = router;
